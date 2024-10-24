980667973eb185b0f819821d6e5b8a2ff7a84879
AlissonFredo <douglasalissonsf@gmail.com>
2024-10-23 21:37:37 -0300

    feat: add PersonageController class and add routes for PersonageController class methods

--- src/core/Main.php
+++ src/core/Main.php
@@ -2,10 +2,25 @@
 
 namespace app\core;
 
+use app\core\Router;
+use app\controllers\PersonageController;
+
 class Main
 {
     static function initialize()
     {
-        echo "Hello Word API";
+        $router = new Router();
+
+        $router->addRoute("POST", "/personages", PersonageController::class, 'create');
+        $router->addRoute("PUT", "/personages", PersonageController::class, 'update');
+        $router->addRoute("GET", "/personages/list", PersonageController::class, 'list');
+        $router->addRoute("GET", "/personages/show", PersonageController::class, 'showById');
+        $router->addRoute("DELETE", "/personages", PersonageController::class, 'destroy');
+        $router->addRoute("GET", "/personages/search", PersonageController::class, 'searchByName');
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        $router->dispatch($method, $uri);
     }
 }
