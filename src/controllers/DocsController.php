431a06c5a65f54110c2cb53e9f489d3f9d5d0d7f
douglas alisson <douglas.alisson@ibridge.com.br>
2024-11-07 20:08:29 -0300

    feat: add Swagger UI to the API so that the user has a better view of the documentation

---
+++ src/controllers/DocsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\controllers;
+
+class DocsController
+{
+    public function docs()
+    {
+        header('Content-Type: text/html');
+
+        $html = file_get_contents(__DIR__ . '/../../vendor/swagger-api/swagger-ui/dist/index.html');
+        $html = preg_replace('/<link\b[^>]*>(.*?)<\/link>/is', '', $html);
+        $html = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $html);
+
+        $customLinks = "
+            <link rel='stylesheet' href='/documentation/assets?filename=swagger-ui.css'>
+            <link rel='stylesheet' href='/documentation/assets?filename=index.css'>
+            <link rel='icon' type='image/png' href='/documentation/assets?filename=favicon-32x32.png' sizes='32x32' />
+            <link rel='icon' type='image/png' href='/documentation/assets?filename=favicon-16x16.png' sizes='16x16' />
+        ";
+
+        $customScripts = "
+            <script src='/documentation/assets?filename=swagger-ui-bundle.js'></script>
+            <script src='/documentation/assets?filename=swagger-ui-standalone-preset.js'></script>
+            <script src='/documentation/assets?filename=swagger-initializer.js'></script>
+            <script>
+                window.onload = function() {
+                    const ui = SwaggerUIBundle({
+                        url: '/api-docs.json', // Certifique-se de que o `swagger.json` está disponível neste caminho
+                        dom_id: '#swagger-ui',
+                        deepLinking: true,
+                        presets: [
+                            SwaggerUIBundle.presets.apis,
+                            SwaggerUIStandalonePreset
+                        ],
+                        layout: 'StandaloneLayout'
+                    });
+                    window.ui = ui;
+                };
+            </script>
+        ";
+
+        $html = preg_replace('/<head\b[^>]*>/is', '$0' . $customLinks, $html, 1);
+        $html = preg_replace('/<\/body>/is', $customScripts . '$0', $html, 1);
+
+        echo $html;
+        exit;
+    }
+
+    public function asset($params)
+    {
+        $filename = $params['filename'] ?? null;
+
+        if (!$filename) {
+            http_response_code(400);
+            return json_encode(['message' => 'Filename is a required attribute']);
+        }
+
+        $path = __DIR__ . "/../../vendor/swagger-api/swagger-ui/dist/$filename";
+
+        if (file_exists($path)) {
+            $extension = pathinfo($path, PATHINFO_EXTENSION);
+
+            switch ($extension) {
+                case 'css':
+                    header('Content-Type: text/css');
+                    break;
+                case 'js':
+                    header('Content-Type: application/javascript');
+                    break;
+                case 'png':
+                    header('Content-Type: image/png');
+                    break;
+                default:
+                    header('Content-Type: text/plain');
+            }
+
+            readfile($path);
+        } else {
+            http_response_code(404);
+            return json_encode(['message' => 'File not found']);
+        }
+    }
+}
