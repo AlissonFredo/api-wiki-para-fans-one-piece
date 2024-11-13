955526ca8c3779749d1cdbc02e31d90df0fcf496
douglas alisson <douglas.alisson@ibridge.com.br>
2024-11-13 00:41:34 -0300

    fix: Add namespace to Controller class and adjust response method to return received parameters

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -1,10 +1,12 @@
 <?php
 
+namespace app\controllers;
+
 class Controller
 {
     protected function response($code, $data)
     {
-        http_response_code(201);
-        return json_encode(['message' => 'Success in creating a character']);
+        http_response_code($code);
+        return json_encode($data);
     }
 }
