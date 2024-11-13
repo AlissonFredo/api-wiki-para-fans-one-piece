79dd3fc7f99b745182628df5a7a8316d09a89c8a
AlissonFredo <douglasalissonsf@gmail.com>
2024-11-13 00:55:48 -0300

    fix: bug that prevents errors from being recorded in the log file

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once('../vendor/autoload.php');
 
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-ini_set('error_log', __DIR__, '/../log/error.log');
+ini_set('error_log', __DIR__ . '/../log/error.log');
 
 use app\core\Main;
 
